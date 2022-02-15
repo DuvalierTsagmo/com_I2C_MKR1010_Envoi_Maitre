489bbc2711fcc4a135c78cf52dbdfca9f0a05a73
duvalier tsagmo <edtsagmo01@monccnb.ca>
2022-02-15 09:36:17 -0400

    mkr1010 qui est le maitre2

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,10 @@
+/*
+  Titre      : communication I2C entre deux uC mkr1000 qui est le maitre
+  Auteur     : Duvalier Tsagmo
+  Date       : 15/02/2022
+  Description: elle permet de faire connecter deux uC qui est le maitre
+  Version    : 0.0.1
+*/
 
 #include <Arduino.h>
 #include <Wire.h>
